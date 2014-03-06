0024248c709ea75932766b164971a3ee0eb306ee
Patrick Arlt <patrick.arlt@gmail.com>
2014-03-06 10:18:15 -0800

    more fixes

--- examples/js-api-directives/app/main.js
+++ examples/js-api-directives/app/main.js
@@ -1,10 +1,7 @@
-// lets require all our apps components before initalizing our app
-require([
-  'angular',
-  'app',
-  'app/controllers/MapCtrl'
-], function(angular) {
-  // since we didn't include ng-app anywhere in our HTML angular hasn't started yet
-  // angular.bootstrap is the same as putting ng-app="app" on the body, but we control when it is called
-  angular.bootstrap(document.body, ['app']);
+define([
+  'angular'
+], function(angular){
+
+  // define our app as an angular module
+  return angular.module("app", []);
 });
